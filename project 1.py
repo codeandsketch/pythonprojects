a99f7e36ba253c64c5f7d51ed3a97f79b31c4858
codeandsketch <aaronnirmal.j@gmail.com>
2024-06-19 16:27:13 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,8 @@
+password= 'incorrect'
+user_pasword=input('what is the password?:  ')
+while user_pasword != password:
+    print('it is incorrect')
+    user_pasword = input('what is the password?:  ')
+
+print('loging in')
+
