eff53d713aeec67e11269380c62cc5378023088c
codeandsketch <aaronnirmal.j@gmail.com>
2024-06-25 16:21:05 +0530

    Add files via upload

---
+++ project 2.py
@@ -0,0 +1,19 @@
+#num=int(input("Number: "))
+# i=1
+# while i<=10:#3x1=3 3x2=6 3x3=9
+#     print(num,"X",i,"=",num*i)
+#     i+=1
+
+
+
+
+
+
+y=1
+while y <= 10:
+    w=1
+    while w <= 10:
+        print(y, 'x', w, '=', y * w)
+        w += 1
+    y += 1
+
